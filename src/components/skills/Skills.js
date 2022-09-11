469dac7336090de8019b3d3d2099b909f212ac61
stvnlee890 <stvnlee890@gmail.com>
2022-09-11 15:54:29 -0700

    changing around skills page'

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -3,11 +3,13 @@ import { Skillset } from './skillset/Skillset'
 
 export const Skills = () => {
   return (
-    <div>
+    <div id='skills-container'>
     <div id='skills'>
-      <h1 className="title-tag active reveal fade-bottom">SKILLS</h1>
+      <h1 className="title-tag active reveal fade-bottom">Skills</h1>
     </div>
+    <div id='skillset-container'>
       <Skillset />
+    </div>
     </div>
   )
 }
--- src/components/skills/skillset/Skillset.js
+++ src/components/skills/skillset/Skillset.js
@@ -11,24 +11,42 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+
 export const Skillset = () => {
+
   return (
     <div>
       <ul id='skill-sets'>
         <li className='skill-set-li'>
-          <span className='skill-num'>001.</span>
-          <span className='skill-titles'>HTML</span>
-          <FontAwesomeIcon className='icons'icon={faHtml5} color="#F06529" />
+          <span className='skill-num hover-underline-animation'>001.</span>
+          <span className='skill-titles'>HTML5</span>
+          {/* <FontAwesomeIcon className='icons hide'icon={faHtml5} color="#F06529" /> */}
         </li>
         <li className='skill-set-li'>
-          <span className='skill-num'>002.</span>
-          <span className='skill-titles'>CSS</span>
-          <FontAwesomeIcon className='icons'icon={faCss3} color="#28A4D9" />
+          <span className='skill-num hover-underline-animation'>002.</span>
+          <span className='skill-titles'>CSS3</span>
+          {/* <FontAwesomeIcon className='icons'icon={faCss3} color="#28A4D9" /> */}
         </li>
         <li className='skill-set-li'> 
-          <span className='skill-num'>003.</span>
+          <span className='skill-num hover-underline-animation'>003.</span>
           <span className='skill-titles'>JavaScript</span>
-          <FontAwesomeIcon className='icons'icon={faJs} color="##F0DB4F" />
+          {/* <FontAwesomeIcon className='icons'icon={faJs} color="#F0DB4F" /> */}
+        </li>
+        <li className='skill-set-li'> 
+          <span className='skill-num hover-underline-animation'>004.</span>
+          <span className='skill-titles'>React</span>
+        </li>
+        <li className='skill-set-li'> 
+          <span className='skill-num hover-underline-animation'>005.</span>
+          <span className='skill-titles'>Python</span>
+        </li>
+        <li className='skill-set-li'> 
+          <span className='skill-num hover-underline-animation'>006.</span>
+          <span className='skill-titles'>Node.js</span>
+        </li>
+        <li className='skill-set-li'> 
+          <span className='skill-num hover-underline-animation'>007.</span>
+          <span className='skill-titles'>Express</span>
         </li>
       </ul>
     </div>
