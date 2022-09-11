07a95f4cd709fc482c84b51b17a2d645330d1e32
stvnlee890 <stvnlee890@gmail.com>
2022-09-11 01:22:41 -0700

    adding skills logos

--- src/components/skills/skillset/Skillset.js
+++ src/components/skills/skillset/Skillset.js
@@ -1,4 +1,15 @@
 import './skillset.css'
+import {
+  faJs,
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+  faPython,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const Skillset = () => {
   return (
@@ -7,14 +18,17 @@ export const Skillset = () => {
         <li className='skill-set-li'>
           <span className='skill-num'>001.</span>
           <span className='skill-titles'>HTML</span>
+          <FontAwesomeIcon className='icons'icon={faHtml5} color="#F06529" />
         </li>
         <li className='skill-set-li'>
           <span className='skill-num'>002.</span>
           <span className='skill-titles'>CSS</span>
+          <FontAwesomeIcon className='icons'icon={faCss3} color="#28A4D9" />
         </li>
         <li className='skill-set-li'> 
           <span className='skill-num'>003.</span>
           <span className='skill-titles'>JavaScript</span>
+          <FontAwesomeIcon className='icons'icon={faJs} color="##F0DB4F" />
         </li>
       </ul>
     </div>
