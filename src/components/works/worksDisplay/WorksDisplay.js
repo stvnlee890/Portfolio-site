7057726906cf798df4b5195f946dcfff6b1f4335
stvnlee890 <stvnlee890@gmail.com>
2022-09-12 20:08:34 -0700

    worked on animation on works component, fixed footer

--- src/components/works/worksDisplay/WorksDisplay.js
+++ src/components/works/worksDisplay/WorksDisplay.js
@@ -1,13 +1,30 @@
 import  Shopkeep from '../../../assets/images/shopkeep.png'
+import Discography from '../../../assets/images/discography.png'
 import './worksDisplay.css'
 
 export const WorksDisplay = () => {
   return (
     <div id='works-display-container'>
-      <img id='final-project-img' 
-        src={Shopkeep} 
-        loading='lazy'
-        alt='final project'/>
+      <div className='works'>
+        <div className='works-title'>
+          <span className='works-num fade-bottom'> 001.</span>
+          <span className='work-title fade-bottom'>DISCOGRAPHY</span>
+        </div>
+        <img id='final-project-img' 
+          src={Discography} 
+          loading='lazy'
+          alt='final project'/>
+      </div>
+      <div className='works'>
+        <div className='works-title'>
+          <span className='works-num fade-bottom'> 002.</span>
+          <span className='work-title fade-bottom'>SHOPKEEP</span>
+        </div>
+        <img id='final-project-img' 
+          src={Shopkeep} 
+          loading='lazy'
+          alt='final project'/>
+      </div>
     </div>
   )
 }
