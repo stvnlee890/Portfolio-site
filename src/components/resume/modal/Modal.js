f234839a43ec2a993c0c57324b9c427954f22fc1
stvnlee890 <stvnlee890@gmail.com>
2022-09-13 14:17:47 -0700

    added resume to modal and moved close button outside

--- src/components/resume/modal/Modal.js
+++ src/components/resume/modal/Modal.js
@@ -1,13 +1,14 @@
 import './modal.css'
-import Discography from '../../../assets/images/discography.png'
+import Resume1 from '../../../assets/images/resume1.png'
+import Resume2 from '../../../assets/images/resume2.png'
 export const Modal = ({ closeModal }) => {
   
   return (
     <div className="modal-background">
-      <div className="modal-container">
         <button className='close-button' onClick={() => closeModal(false)}> X </button>
-          <img className="body" id='img' src='https://resumegenius.com/wp-content/uploads/Retail-Manager-Resume-Example-v5-800x1035.png' />
-          <img className="body" id='img' src='https://resumegenius.com/wp-content/uploads/Retail-Manager-Resume-Example-v5-800x1035.png' />
+      <div className="modal-container">
+          <img className="body" id='img' src={Resume1} />
+          <img className="body" id='img' src={Resume2} />
       </div>
     </div>
   )
