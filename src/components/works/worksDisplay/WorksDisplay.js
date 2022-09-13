61097ed0cfd125d2a6bcf53b816ad55d3247a71c
stvnlee890 <stvnlee890@gmail.com>
2022-09-12 20:55:05 -0700

    working more on works component

--- src/components/works/worksDisplay/WorksDisplay.js
+++ src/components/works/worksDisplay/WorksDisplay.js
@@ -10,21 +10,28 @@ export const WorksDisplay = () => {
           <span className='works-num fade-bottom'> 001.</span>
           <span className='work-title fade-bottom'>DISCOGRAPHY</span>
         </div>
-        <img id='final-project-img' 
+        {/* <a href='https://music-discography.netlify.app/'>
+        <img className='final-project-img' id='discog'
           src={Discography} 
-          loading='lazy'
           alt='final project'/>
+        </a> */}
+        <a href='https://music-discography.netlify.app/'>
+        <div className='discog'></div>
+        </a>
       </div>
       <div className='works'>
         <div className='works-title'>
           <span className='works-num fade-bottom'> 002.</span>
           <span className='work-title fade-bottom'>SHOPKEEP</span>
         </div>
-        <img id='final-project-img' 
-          src={Shopkeep} 
-          loading='lazy'
-          alt='final project'/>
+        <a href='https://shopkepp.netlify.app/'>
+        {/* <img className='final-project-img' id='shopkeep'
+          src={Shopkeep}
+          alt='final project'/> */}
+          <div className='shopkeep'></div>
+        </a>
       </div>
     </div>
   )
 }
+
