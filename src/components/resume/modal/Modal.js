2bfbd71d40cbc03191f748e8ad837f4a22447062
stvnlee890 <stvnlee890@gmail.com>
2022-09-14 11:04:04 -0700

    cleaning up code

--- src/components/resume/modal/Modal.js
+++ src/components/resume/modal/Modal.js
@@ -7,8 +7,8 @@ export const Modal = ({ closeModal }) => {
     <div className="modal-background">
         <button className='close-button' onClick={() => closeModal(false)}> X </button>
       <div className="modal-container">
-          <img className="body" id='img' src={Resume1} />
-          <img className="body" id='img' src={Resume2} />
+          <img className="body" id='img' alt='resume page 1' src={Resume1} />
+          <img className="body" id='img' alt='resume page 2' src={Resume2} />
       </div>
     </div>
   )
