96ce7ac68319571b987b288a0f920547472ea500
stvnlee890 <stvnlee890@gmail.com>
2022-09-13 17:53:44 -0700

    added more skills

--- src/components/skills/skillset/Skillset.js
+++ src/components/skills/skillset/Skillset.js
@@ -36,6 +36,18 @@ export const Skillset = () => {
           <span className='skill-num hover-underline-animation'>007.</span>
           <span className='skill-titles'>Express</span>
         </li>
+        <li className='skill-set-li'> 
+          <span className='skill-num hover-underline-animation'>008.</span>
+          <span className='skill-titles'>SQL</span>
+        </li>
+        <li className='skill-set-li'> 
+          <span className='skill-num hover-underline-animation'>009.</span>
+          <span className='skill-titles'>PSQL</span>
+        </li>
+        <li className='skill-set-li'> 
+          <span className='skill-num hover-underline-animation'>010.</span>
+          <span className='skill-titles'>MongoDB</span>
+        </li>
       </ul>
     </div>
   )
