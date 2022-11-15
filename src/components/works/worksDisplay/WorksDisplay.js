3e8f064a54359897e891ab798ecf2500bf426ee8
stvnlee890 <stvnlee890@gmail.com>
2022-11-15 11:34:30 -0800

    updated resume, updated new project

--- src/components/works/worksDisplay/WorksDisplay.js
+++ src/components/works/worksDisplay/WorksDisplay.js
@@ -11,6 +11,7 @@ export const WorksDisplay = () => {
           <a href='https://music-discography.netlify.app/'>
             <div className='discog'></div>
           </a>
+          <span className='bottom fade-bottom'>REACT | CSS</span>
       </div>
       <div className='works'>
         <div className='works-title'>
@@ -20,6 +21,17 @@ export const WorksDisplay = () => {
           <a href='https://shopkepp.netlify.app/'>
             <div className='shopkeep'></div>
           </a>
+          <span className='bottom fade-bottom'>MERN STACK | AWS</span>
+      </div>
+      <div className='works'>
+        <div className='works-title'>
+          <span className='works-num fade-bottom'> 003.</span>
+          <span className='work-title fade-bottom'>STORYPOST</span>
+        </div>
+          <a href='https://storypost.netlify.app/'>
+            <div className='storypost'></div>
+          </a>
+          <span className='bottom fade-bottom'>MERN STACK | AWS | TAILWIND</span>
       </div>
     </div>
   )
